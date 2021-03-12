b1e1d20fac060016148687a2d6e347497e91d4f6
bingyang001 <bingyang136@163.com>
2021-03-12 18:55:26 +0800

    update dubbo pom

--- hummer-archetype-spring-boot-dubbo/src/main/resources/archetype-resources/__rootArtifactId__-api/src/main/java/api/controller/SimpleController.java
+++ hummer-archetype-spring-boot-dubbo/src/main/resources/archetype-resources/__rootArtifactId__-api/src/main/java/api/controller/SimpleController.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -57,5 +58,15 @@ public class SimpleController {
     public ResourceResponse<Integer> add(@PathVariable("a") @Valid @Min(value = 0, message = "min 0") int a
             , @PathVariable("b") @Valid @Min(value = 0, message = "min 0") int b) {
         return ResourceResponse.ok(otherService.add(a, b));
+    }
+
+    @GetMapping("/add2/{a}/{b}")
+    @ApiOperation(value = "this is add demo")
+    public ResourceResponse<Integer> add2(@PathVariable("a") @Valid @Min(value = 0, message = "min 0") Integer a
+            , @PathVariable("b") @Valid @Min(value = 0, message = "min 0") Integer b) {
+        SimpleDubboDto dto = new SimpleDubboDto();
+        dto.setA(a);
+        dto.setB(b);
+        return ResourceResponse.ok(otherService.add2(dto));
     }
 }
--- hummer-archetype-spring-boot-dubbo/src/main/resources/archetype-resources/__rootArtifactId__-integration/src/main/java/integration/SimpleOtherService.java
+++ hummer-archetype-spring-boot-dubbo/src/main/resources/archetype-resources/__rootArtifactId__-integration/src/main/java/integration/SimpleOtherService.java
@@ -1,5 +1,10 @@
 package ${package}.integration;
 
+import comm.hummer.simple.common.facade.HelloService;
+import comm.hummer.simple.common.module.SimpleDubboDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 /**
  * this class impl call depend remote service
  */
@@ -11,5 +16,9 @@ public class SimpleOtherService {
 
     public int add (int a,int b){
         return helloService.add(a,b);
+    }
+
+    public int add2(SimpleDubboDto dto){
+        return helloService.add2(dto);
     }
 }
