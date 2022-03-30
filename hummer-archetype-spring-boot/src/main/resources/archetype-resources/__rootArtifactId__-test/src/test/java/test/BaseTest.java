e1005d7963ee74afe1c4a08fee0835074917155b
guo.li <guo.li@accenture.com>
2022-03-30 18:18:11 +0800

    add check style

--- hummer-archetype-spring-boot/src/main/resources/archetype-resources/__rootArtifactId__-test/src/test/java/test/BaseTest.java
+++ hummer-archetype-spring-boot/src/main/resources/archetype-resources/__rootArtifactId__-test/src/test/java/test/BaseTest.java
@@ -9,12 +9,18 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(value = SpringRunner.class)
 @Import(value = {ApplicationContextInitBean.class, ExportDaoInitBean.class})
 @ContextConfiguration(initializers =
         {ConfigFileApplicationContextInitializer.class, HummerApplicationContextInit.class})
 @ComponentScan(basePackages = "${package}")
+//@HummerRestApiClientBootScan(scanBasePackages = "cn.com.microsoftstore.thrift.store.api.integration")
+@WebAppConfiguration
 public class BaseTest {
+    @Test
+    public void bootstrap() {
 
+    }
 }
