f02d8cd79d3897f9aaa9d7e3263917b8d4e17de3
guo.li <1qaz@WSX>
2022-05-10 15:07:06 +0800

    update chekStyle

--- hummer-archetype-spring-boot/src/main/resources/archetype-resources/__rootArtifactId__-test/src/test/java/test/BaseTest.java
+++ hummer-archetype-spring-boot/src/main/resources/archetype-resources/__rootArtifactId__-test/src/test/java/test/BaseTest.java
@@ -3,6 +3,7 @@ package ${package}.test;
 import com.hummer.core.ApplicationContextInitBean;
 import com.hummer.core.init.HummerApplicationContextInit;
 import com.hummer.dao.starter.ExportDaoInitBean;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer;
 import org.springframework.context.annotation.ComponentScan;
