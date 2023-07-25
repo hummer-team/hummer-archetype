481817386bb97403b99fdc265edf54eb392d9458
guo.li <bingyang136@163.com>
2023-07-25 15:22:07 +0800

    upgrade hummer framework version

--- hummer-archetype-spring-boot/src/main/resources/archetype-resources/__rootArtifactId__-test/src/test/java/test/BaseTest.java
+++ hummer-archetype-spring-boot/src/main/resources/archetype-resources/__rootArtifactId__-test/src/test/java/test/BaseTest.java
@@ -8,6 +8,7 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -17,7 +18,9 @@ import org.springframework.test.context.web.WebAppConfiguration;
 @ContextConfiguration(initializers =
         {ConfigFileApplicationContextInitializer.class, HummerApplicationContextInit.class})
 @ComponentScan(basePackages = "${package}")
-//@HummerRestApiClientBootScan(scanBasePackages = "cn.com.microsoftstore.thrift.store.api.integration")
+@PropertySource({"classpath:application.properties","classpath:application-dev.properties"})
+//If using the hummer rest client wrapper framework, enable this configuration
+//@HummerRestApiClientBootScan(scanBasePackages = "xx")
 @WebAppConfiguration
 public class BaseTest {
     @Test
